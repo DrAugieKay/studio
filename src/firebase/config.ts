052dcb6156f856f7d18f86ba37c835daa8f55679
Augustine K. Anokye-Wusu <GastyA11@gmail.com>
2025-11-07 21:53:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2240130852-c09ed",
+  "appId": "1:804982017883:web:f23d38b409a750251f947e",
+  "apiKey": "AIzaSyAsI6pi7mJesXfuQNHbQGi9Kz3oaqh00nw",
+  "authDomain": "studio-2240130852-c09ed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "804982017883"
+};
